32c245fcbe51a01b77321d4c4c68faa74c906342
Sylvain <s911141111@gmail.com>
2014-06-19 09:31:50 +0200

    Added initial scale for mobile devices

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 <head>
 	<meta charset="utf-8">
 	<title>Piwake Lite</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="icon" type="image/png" href="img/favicon.png" />
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	<link href="css/style.css" rel="stylesheet">
